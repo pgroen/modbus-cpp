40c6b751f30e507324daa02de78b0e9bbe1c2504
Peter M. Groen <pgroen@nuc64.osdev.nl>
2022-12-25 22:37:04 +0100

    Implement RTU Unittest

--- src/modbusbase.h
+++ src/modbusbase.h
@@ -57,8 +57,8 @@ namespace modbus {
 class ModbusBase
 {
 public:
-    ModbusBase();
-    virtual ~ModbusBase();
+    ModbusBase() {}
+    virtual ~ModbusBase() {}
 
     // Pure virtuals. Override when inherited.
     virtual bool Connect() = 0;
---
+++ tests/connectionconfigtest.cpp
@@ -0,0 +1,27 @@
+/****************************************************************
+ * Copyright (c)2022 Peter M. Groen
+ * This file is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ ****************************************************************/
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "connectionconfig.h"
+
+using namespace osdev::components::modbus;
+
+TEST(ConnectionConfigTest, SerialPortConfig)
+{
+    ConnectionConfig oConfig;
+    oConfig.setBaudRate( B9600 );
+    oConfig.setConnectionType( ConnectionConfig::E_CONNECTIONTYPE::SERIAL );
+    oConfig.setDataBits( 8 );
+    oConfig.setStopBits( 1 );
+    oConfig.setFrameTimeout( 10 );
+    oConfig.setParity( ConnectionConfig::E_PARITY::NONE );
+    oConfig.setPortName( "/dev/ttyUSB0" );
+
+    // Test all parameters
+
+}
